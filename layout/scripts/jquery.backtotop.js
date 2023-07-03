f43f998875f052e50d161cf3923f65d48b15e0a5
Teddy Okatch <okatch.teddycars@gmail.com>
2023-07-04 02:01:28 +0300

    2nd commit

--- layout/scripts/jquery.backtotop.js
+++ layout/scripts/jquery.backtotop.js
@@ -1,11 +1,5 @@
 /*
-Template Name: Besloor
-Author: <a href="https://www.os-templates.com/">OS Templates</a>
-Author URI: https://www.os-templates.com/
-Copyright: OS-Templates.com
-Licence: Free to use under our free template licence terms
-Licence URI: https://www.os-templates.com/template-terms
-File: Back to Top JS
+
 */
 
 jQuery("#backtotop").click(function () {
